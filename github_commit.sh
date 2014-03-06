fbd7cc6cb12bf988f6ca9b5c5594dac1315c9d61
wendellyi <wendellyi@yeah.net>
2014-03-06 10:00:17 +0800

    1.修改提交脚本

--- github_commit.sh
+++ github_commit.sh
@@ -1,8 +1,8 @@
 #! /bin/bash
 
-COMMIT_PTR=$$
+COMMIT_PTR=$1
 
-git add .
-git commit
-git remote add $COMMIT_PTR https://github.com/wendellyi/emacs.git
+env GIT_SSL_NO_VERIFY=true git add .
+env GIT_SSL_NO_VERIFY=true git commit
+env GIT_SSL_NO_VERIFY=true git remote add $COMMIT_PTR https://github.com/wendellyi/emacs.git
 env GIT_SSL_NO_VERIFY=true git push $COMMIT_PTR master
